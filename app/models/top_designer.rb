70a26061d4255276e6c39c3a2c3cbda687c9ed2b
Ian Vidaurre <ianjvidaurre@gmail.com>
2018-06-05 15:47:39 -0400

    Updated application controller.

--- app/models/top_designer.rb
+++ app/models/top_designer.rb
@@ -1,2 +1,5 @@
 class TopDesigner < ApplicationRecord
+    def self.search(search)
+        where("first_name ILIKE ? OR last_name ILIKE ? OR company_name ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+    end
 end
